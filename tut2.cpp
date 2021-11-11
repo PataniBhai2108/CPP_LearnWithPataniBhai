7e692a9fd2e19b8f4034bda2f68da09841ef8670
PataniBhai2108 <patanibhaiawesome@gmail.com>
2021-11-12 01:47:01 +0530

    Tutorial 2

---
+++ tut2.cpp
@@ -0,0 +1,22 @@
+/*
+ 
+CREATOR: PATANI BHAI YOUTUBE CHANNEL
+CREATION AT:  11th November, 2021.
+TOPICS COVERED: 
+  1.) Hello World code working explained and made better.
+  2.) Code Runner setting and trial.
+ 
+*/
+ 
+// *************** CODE START ***************
+ 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+  cout << "Hello World";
+  
+  return 0;
+}
+
+// *************** THE END ***************
