5341db189f9ae561f9e3de782a5b3d1f87fedce7
PataniBhai2108 <patanibhaiawesome@gmail.com>
2021-11-12 01:47:34 +0530

    Tutorial 3

---
+++ tut3.cpp
@@ -0,0 +1,23 @@
+/*
+ 
+CREATOR: PATANI BHAI YOUTUBE CHANNEL
+CREATION AT:  11th November, 2021.
+TOPICS COVERED: 
+  1.) Comments and
+  2.) Variables.
+ 
+*/
+ 
+// *************** CODE START ***************
+ 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+  int age = 22; // variable name age with value of 22.
+  cout << "Age = " << age; // displaying the age using cout functionality.
+  
+  return 0;
+}
+ 
+// *************** THE END ***************
