b1b0f5507258df9f28b8cfa1a90e2ce1770940bf
PataniBhai2108 <patanibhaiawesome@gmail.com>
2021-11-12 01:48:53 +0530

    Tutorial 5

---
+++ tut5.cpp
@@ -0,0 +1,34 @@
+/*
+ 
+CREATOR: PATANI BHAI YOUTUBE CHANNEL
+CREATION AT:  11th November, 2021.
+TOPICS COVERED: 
+  1.) Input,
+  2.) Output and
+  3.) End Line.
+ 
+*/
+ 
+// *************** CODE START ***************
+ 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+  // We are declaring two variable of the same type(integer).
+  int num1, num2;
+  /*
+  << => Insertion Operator.
+  >> => Extraction Operator.
+  endl => End line to display the next command on a new line
+  */
+  cout << "Enter the value of num1: " << endl;
+  cin >> num1;
+  cout << "Enter the value of num2: " << endl;
+  cin >> num2;
+  cout << "The sum is "<< num1+num2 << endl;
+  
+  return 0;
+}
+ 
+// *************** THE END ***************
