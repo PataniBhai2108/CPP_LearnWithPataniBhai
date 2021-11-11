daa5d768048ccfa91b7bab48bd55c7640c8cdd31
PataniBhai2108 <patanibhaiawesome@gmail.com>
2021-11-12 01:46:12 +0530

    Tutorial 1

---
+++ tut1.cpp
@@ -0,0 +1,20 @@
+/*
+ 
+CREATOR: PATANI BHAI YOUTUBE CHANNEL
+CREATION AT: 11th November, 2021.
+TOPICS COVERED: 
+  C++ code boiler plate creation and simple "Hello World" code test run.
+
+*/
+ 
+// *************** CODE START ***************
+ 
+#include <iostream>
+ 
+int main() {
+  std::cout << "Hello World";  
+  
+  return 0;
+}
+ 
+// *************** THE END ***************
